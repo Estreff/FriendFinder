e3373aec63ac77a082ae963a727de3f039aa9746
Erik Streff <Estreff@gmail.com>
2017-10-16 18:07:12 -0600

    Final update with calculations

---
+++ app/data/friends.js
@@ -0,0 +1,81 @@
+    var friends = [{
+        "name":"Han Solo",
+        "image":"https://www.google.com/search?q=han+solo&source=lnms&tbm=isch&sa=X&ved=0ahUKEwja7Z6k0PDWAhWL5oMKHcMwA70Q_AUICigB&biw=1366&bih=637",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },{
+        "name":"Luke Skywalker",
+        "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO_izSSqg7eHAcaONKgMboTmP5VaXZIaFIvxNj_R5B3R5bXDTpOA",
+        "scores":[
+            4,
+            2,
+            3,
+            4,
+            3,
+            2,
+            3,
+            3,
+            2,
+            5
+        ]
+    },{
+        "name":"Chewbacca",
+        "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8T3n6H4F-M5f44f1ocKpg8MaIVDqOSBj2idEIONSXUnItifYOxw",
+        "scores":[
+            2,
+            2,
+            1,
+            4,
+            3,
+            2,
+            1,
+            3,
+            4,
+            2
+        ]
+    },{
+        "name":"Princess Leia",
+        "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKQ3UhFex61_jwHEWKYWpua2mvlljnaks3GGBiNRHx7Mqop4A_hQ",
+        "scores":[
+            4,
+            2,
+            3,
+            4,
+            3,
+            2,
+            3,
+            3,
+            2,
+            5
+        ]
+    },{                      
+        "name":"Darth Maul",
+        "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReGK_T7f8m2drpNdEVXWH9H_mrxi6BowU2GITxhtrfVR0209aN",
+        "scores":[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+            ]
+        }
+
+    
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,20 @@
+var bodyParser = require('body-parser');
+var path = require('path');
+var express = require('express');
+var app = express();
+port =  process.env.port || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( { extended: true } ));
+app.use(bodyParser.text());
+app.use(bodyParser.json( { type: "application/vnd.api+json" } ));
+
+
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+app.listen(port, function() {
+    console.log('Listening on port ' + port);
+});
