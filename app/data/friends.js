792ebd0ddd9c8ee441662b3b48243221e5a59318
Erik Streff <Estreff@gmail.com>
2017-10-17 11:04:58 -0600

    Updated the required fields

--- app/data/friends.js
+++ app/data/friends.js
@@ -47,16 +47,16 @@
         "name":"Princess Leia",
         "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKQ3UhFex61_jwHEWKYWpua2mvlljnaks3GGBiNRHx7Mqop4A_hQ",
         "scores":[
-            4,
-            2,
+            3,
+            1,
             3,
             4,
             3,
-            2,
+            1,
             3,
             3,
             2,
-            5
+            3
         ]
     },{                      
         "name":"Darth Maul",
